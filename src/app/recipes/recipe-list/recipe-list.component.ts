6e5930a95cef3158872f97053baf9b577b698a13
= <michal@debian>
2017-09-18 14:46:25 +0200

    shopping-list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('A test recipe', 'This is test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+        new Recipe('A test recipe', 'This is test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
